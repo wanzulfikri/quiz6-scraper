5960bca8dd0ad93d5a65904faf773475b636b41a
wanzulfikri <wanzulfikri@gmail.com>
2020-05-06 02:25:00 +0800

    Add questions 1-59 to the vault

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ import (
 // TODO: return error if error
 func isExtraneousInfo(text string, list []string) bool {
 	for _, el := range list {
-		// fmt.Println(strings.Contains(text, el))
-		// fmt.Printf("%s %s", text, el)
 		if strings.Contains(text, el) {
 			return true
 		}
@@ -79,8 +77,6 @@ func main() {
 		e.ForEach("tr", func(count int, e *colly.HTMLElement) {
 			text := e.Text
 			if !isExtraneousInfo(text, extraneous) {
-				// trimmedText := strings.TrimSpace(text)
-				// trimmedText = strings.ReplaceAll(trimmedText, " ", "")
 				answers = append(answers, text)
 			}
 		})
@@ -91,7 +87,7 @@ func main() {
 	})
 
 	// max question number is 1060
-	for min, max, increment := 61, 70, 10; max <= 1060; {
+	for min, max, increment := 1, 10, 10; max <= 1060; {
 		url := fmt.Sprintf("https://www.softwaretestinggenius.com/istqb-certification-exam-sample-papers-q-%d-to-%d/", min, max)
 		c.Visit(url)
 		min = min + increment
